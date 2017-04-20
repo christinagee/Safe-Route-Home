3718de7f461b1c1be307ca49de6bf0925711e474
FrancesDevanbu <frances.devanbu@students.olin.edu>
2017-04-19 23:58:17 -0400

    api

---
+++ Map-Quest_Get.py
@@ -0,0 +1,67 @@
+import requests
+import json
+from pickle import dump , load
+import sys
+
+
+
+
+class Send_Data:
+    """ class that creates the data to send to mapquestapi
+    and converts it with Json"""
+
+    def __init__(self, start, end, points):
+        self.start = start
+        self.end = end
+        self.Data()
+        self.data['routeControlPointCollection'] = 
+        self.data= json.dumps(self.data)
+        self.Get_Directions()
+
+
+    def Data(self):
+        self.data={}
+        self.data['locations'] = [self.start, self.end]
+        self.Options()
+        self.data['options']= self.options
+
+    def Options(self):
+        self.options ={}
+        self.options['avoids'] = []
+        self.options['avoidTimedConditions'] = False
+        self.options['doReverseGeocode'] = True
+        self.options['shapeFormat'] = 'raw'
+        self.options['generalize'] = 0
+        self.options['routeType'] = 'fastest'
+        self.options['timeType'] = 1
+        self.options['locale'] = 'en_US'
+        self.options['unit'] = 'm'
+        self.options['enhancedNarratives'] = False
+        self.options['drivingStyle'] = 2
+        self.options['highwayEfficiency'] = 21
+
+    def RouteControlPoints(self):
+        pass
+
+    def Get_Directions(self):
+        url='http://www.mapquestapi.com/directions/v2/route?key=myLzT3Tf3PjQVNlYNuPhVCU7jxBP0wVG'
+        self.response = requests.post(url, data=self.data)
+        self.response = self.response.json()
+
+    def Sort_Directions(self):
+        pass
+
+
+    def Store_Directions(self):
+        Directions = open(Directions,'wb')
+        dump(self.directions,Directions)
+        Directions.close()
+
+if __name__ == '__main__':
+    mydata = Send_Data("Clarendon Blvd, Arlington, VA","2400 S Glebe Rd, Arlington, VA",)
+    #response = requests.post(url, data=mydata.data)
+    #print (response.json())
+    #response = response.json()
+    print (mydata.response['route']['legs'])
+
+    print (mydata.data)
